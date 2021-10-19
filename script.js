c85308f051b855536639cfb3c00d944824566246
TolikaTolika123 <tolikatolika794@gmail.com>
2021-10-19 14:37:39 +0400

    added rounds counter

--- script.js
+++ script.js
@@ -8,89 +8,102 @@ const playerScore = document.querySelector('.player__score');
 const computerScore = document.querySelector('.computer__score');
 const computerImageContainer = document.querySelector('.image-big.computer');
 const playerImageContainer = document.querySelector('.image-big.player');
+const rounds = document.querySelector('.rounds');
 
-let maxRounds = parseInt(prompt('How many rounds do you want to play?', 0));
+let maxRounds = parseInt(prompt('How many rounds do you want to play?', 1));
 let possibleMoves = ['rock', 'paper', 'scissors'];
 let roundsWon = 0;
 let roundsLost = 0;
-
-counter.innerHTML = 1;
+rounds.innerHTML = maxRounds;
+let innerCounter = 1;
+counter.innerHTML = innerCounter;
 function computerPlay() {
   return possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  
-  if (counter.innerHTML == maxRounds) {
-    if (roundsWon > roundsLost) {
-      alert('You won!');
-    } else if (roundsLost > roundsWon) {
-      alert('You lost!');
-    } else if (roundsWon == roundsLost) {
-      alert('It\'s a tie');
-    }
-  }
-  counter.innerHTML++;
-
 
   if (playerSelection == 'rock') {
     if (computerSelection == 'rock') {
       computerImageContainer.innerHTML = '<img src="images/rock.svg" class="big__image">';
     } else if (computerSelection == 'scissors') {
-      roundsWon++;
       computerImageContainer.innerHTML = '<img src="images/scissors.svg" class="big__image">';
+      roundsWon++;
     } else if (computerSelection == 'paper') {
-      roundsLost++;
       computerImageContainer.innerHTML = '<img src="images/paper.svg" class="big__image">';
+      roundsLost++;
     }
   } else if (playerSelection == 'paper') {
     if (computerSelection == 'rock') {
-      roundsWon++;
       computerImageContainer.innerHTML = '<img src="images/rock.svg" class="big__image">';
+      roundsWon++;
     } else if (computerSelection == 'scissors') {
-      roundsLost++;
       computerImageContainer.innerHTML = '<img src="images/scissors.svg" class="big__image">';
+      roundsLost++;
     } else if (computerSelection == 'paper') {
       computerImageContainer.innerHTML = '<img src="images/paper.svg" class="big__image">';
     }
   } else if (playerSelection == 'scissors') {
     if (computerSelection == 'rock') {
-      roundsLost++;
       computerImageContainer.innerHTML = '<img src="images/rock.svg" class="big__image">';
+      roundsLost++;
     } else if (computerSelection == 'scissors') {
       computerImageContainer.innerHTML = '<img src="images/scissors.svg" class="big__image">';
     } else if (computerSelection == 'paper') {
-      roundsWon++;
       computerImageContainer.innerHTML = '<img src="images/paper.svg" class="big__image">';
+      roundsWon++;
     }
   }
+
+  playerScore.innerHTML = roundsWon;
+  computerScore.innerHTML = roundsLost;
+  innerCounter++;
+  counter.innerHTML = innerCounter;
 }
 
 // adding click functions to buttons
 
-paper.addEventListener('click', function() {
-  let computerSelection = computerPlay();
-  console.log(computerSelection);
-  playRound('paper', computerSelection);
-  playerScore.innerHTML = roundsWon;
-  computerScore.innerHTML = roundsLost;
+paper.addEventListener('click', function () {
   playerImageContainer.innerHTML = '<img src="images/paper.svg" class="big__image">';
+  let computerSelection = computerPlay();
+  playRound('paper', computerSelection);
+  if (counter.innerHTML > maxRounds) counter.innerHTML = maxRounds;
+  setTimeout(isGameOver, 100);
 });
 
-rock.addEventListener('click', function() {
-  let computerSelection = computerPlay();
-  console.log(computerSelection);
-  playRound('rock', computerSelection);
-  playerScore.innerHTML = roundsWon;
-  computerScore.innerHTML = roundsLost;
+rock.addEventListener('click', function () {
   playerImageContainer.innerHTML = '<img src="images/rock.svg" class="big__image">';
+  let computerSelection = computerPlay();
+  playRound('rock', computerSelection);
+  if (counter.innerHTML > maxRounds) counter.innerHTML = maxRounds;
+  setTimeout(isGameOver, 100);
 });
 
-scissors.addEventListener('click', function() {
-  let computerSelection = computerPlay();
-  console.log(computerSelection);
-  playRound('scissors', computerSelection);
-  playerScore.innerHTML = roundsWon;
-  computerScore.innerHTML = roundsLost;
+scissors.addEventListener('click', function () {
   playerImageContainer.innerHTML = '<img src="images/scissors.svg" class="big__image">';
+  let computerSelection = computerPlay();
+  playRound('scissors', computerSelection);
+  if (counter.innerHTML > maxRounds) counter.innerHTML = maxRounds;
+  setTimeout(isGameOver, 100);
 });
+
+// Rounds restaring function
+
+function isGameOver() {
+  if (innerCounter > maxRounds) {
+    if (roundsWon > roundsLost) {
+      alert('You won! Do you want to play again?');
+    } else if (roundsLost > roundsWon) {
+      alert('You lost! Do you want to play again?');
+    } else if (roundsWon == roundsLost) {
+      alert(`It's a tie! Do youwant to play again?`);
+    }
+    roundsWon = 0;
+    roundsLost = 0;
+    playerScore.innerHTML = roundsWon;
+    computerScore.innerHTML = roundsLost;
+    playerImageContainer.innerHTML = '';
+    computerImageContainer.innerHTML = '';
+    innerCounter = 1;
+  }
+}
